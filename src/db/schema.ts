91d1056f6661e87211bad46e2cad9a09c8d277fc
Ilija <ilijakoske@gmail.com>
2023-10-09 22:07:54 +0200

    First commit

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
+
+export const todos = sqliteTable("todos", {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    content: text("content").notNull(),
+    completed: integer("completed", {mode: "boolean"}).notNull().default(false),
+});
+
+export type TodoSelect = InferSelectModel<typeof todos>;
+export type TodoInsert = InferInsertModel<typeof todos>;
